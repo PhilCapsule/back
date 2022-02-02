4f09a30c3e44c2b74999639a8f0461b70fdb9f51
Phil <phildev013@gmail.com>
2022-02-02 14:34:00 +0100

    first commit page accueil

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+const res = require('express/lib/response');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+/* Accueil */ 
+
+router.get('/accueil', function(req,res,next){
+  res.render('accueil')
+})
+
 
 module.exports = router;
